1671deb660e9e581e79db27a0929000e9c718e4d
AurRazex <AlekSevrus@gmail.com>
2024-07-27 16:43:36 +0300

    Ver.1 Test_4

--- src/test/java/UiAbstractTest.java
+++ src/test/java/UiAbstractTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class UiAbstractTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class UiAbstractTest {
+    static ChromeOptions options;
+
+    @BeforeAll
+    static void init(){
+        WebDriverManager.chromedriver().setup();
+        options = new ChromeOptions();
+        options.addArguments("--incognito");
+        //options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        options.addArguments("--remote-allow-origins=*");
+    }
+    public ChromeOptions getOptions(){
+        return options;
+    }
 }
